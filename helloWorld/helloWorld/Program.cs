7319f334a852e068097351d1ee78aaac8c484fca
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-26 13:52:22 -0600

    tipos de dato

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -7,6 +7,18 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            //Tipos de dato
+
+            // int - 4 bytes, almacena informacion de 2,147,483,648 hasta 2,147,483,647
+            // bool - 1 bit almacena informacion de 0 a 1
+            // float - 4 bytes, hasta 6 a 7 digitos
+            // double - 8 bytes numeros decimales hasta 15 digitos
+            // char - 2 bytes almacena un caracter o letra
+            // string - 2 bytes por caracter, se almacena una secuencia de caracteres
+            // long - -9223372036854775808 to 9223372036854775807
+            // uint 0 hasta 4,294,967,295
         }
     }
 }
