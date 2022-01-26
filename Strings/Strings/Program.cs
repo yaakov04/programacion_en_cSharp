279aa781567b5befb6984dca4843c082a2770276
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-26 14:04:08 -0600

    strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int height = 180;
+int age = 27;
+string name = "Diego Briones";
+
+Console.WriteLine("Hola. \nMi nombre es " + name 
+    + "\nmi altura es " + height + " centimetros \nmi edad es " + age);
