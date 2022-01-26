01f917549eef81acb94ea61af56b29532f7b2f16
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-26 13:42:20 -0600

    Variables

---
+++ NumericCalculator/NumericCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number1;
+int number2;
+
+Console.WriteLine("Input the first number");
+
+//casteo: convercion de tipos. En este caso de string a int
+number1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Input the second number");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 * number2;
+
+Console.WriteLine("The result is: " + result);
+
+
---
+++ StringExample/StringExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name");
+            string fullUserName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + fullUserName);
+        }
+
+    }
+}
