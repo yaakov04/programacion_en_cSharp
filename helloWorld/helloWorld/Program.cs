3457e8b972bd9378fbeef6728bc7d447b1439b5e
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-26 12:45:09 -0600

    hola mundo

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
