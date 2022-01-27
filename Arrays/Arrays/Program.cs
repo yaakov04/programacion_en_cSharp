fdf1768bb5e392f6435f796c75d95dad95216fa4
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-26 19:50:36 -0600

    arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Arreglos \n");
+
+string[] coffeTypes;
+float[] coffePrices;
+
+coffeTypes = new string[] { "Expresso", "Largo", "Filtrado", "Latte" };
+coffePrices = new float[] {1.2f, 1.5f, 5.0f, 5.5f };
+
+coffeTypes[1] = "Lungo";
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine(coffeTypes[i] + " coffe $" + coffePrices[i]);
+}
