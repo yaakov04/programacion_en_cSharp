13f8f3db7c8a6d09d38738f9f3375876f7af58b5
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-27 10:05:28 -0600

    metodos

---
+++ MyMethods1/MyMethods1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyMethods1
+{
+    internal class MyMethods1
+    {
+        public void Sum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("El resultado de la suma es: "+result);
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creando mis propios metodos\n");
+
+            MyMethods1 myProgram = new MyMethods1();
+
+            myProgram.Sum(1, 2);
+            int mult = myProgram.Mult(2, 5);
+            Console.WriteLine("\nEl resultado de la multiplicacion es: " + mult);
+
+        }
+    }
+}
---
+++ MyMethods2/MyMethods2/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Creando metodos 2\n");
+
+void printName (String name)
+{
+    Console.WriteLine(name);
+}
+
+printName("Diego Briones");
+
+Console.WriteLine("\nCalculadora\n");
+
+int sum (int x, int y)
+{
+    return x + y;
+}
+
+int res(int x, int y)
+{
+    return x - y;
+}
+
+int mult(int x, int y)
+{
+    return x * y;
+}
+
+int div(int x, int y)
+{
+    return x / y;
+}
+
+int resultDiv = div(10, 2);
+Console.WriteLine("La division es: " + resultDiv);
+
+int resultMult = mult(10, 2);
+Console.WriteLine("La multiplicacion es: " + resultMult);
+
+int resultRes = res(10, 2);
+Console.WriteLine("La resta es: " + resultRes);
+
+int resultSum = sum(10, 2);
+Console.WriteLine("La suma es: " + resultSum);
