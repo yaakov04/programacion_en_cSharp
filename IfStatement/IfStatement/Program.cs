2dd22b1cc3a10ca9ff4bfcdb2791c63f8e66656a
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-27 10:50:43 -0600

    if y switch

---
+++ IfStatement/IfStatement/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("El condicional");
+
+Random rnd = new Random();
+
+int randomValue = rnd.Next(6, 15);
+Console.WriteLine("el numero es: "+ randomValue);
+string message = "";
+
+if (randomValue == 7)
+{
+    message = "El valor es 7";
+}
+else if(randomValue == 14)
+{
+    message = "El valor es 14";
+}
+else
+{
+    message = "El valor no es 7";
+}
+
+Console.WriteLine(message);
---
+++ SwitchExample/SwitchExample/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Switch");
+
+List<string> sodaList = new List<string>
+{
+    "Coca - Cola",
+    "Sprite",
+    "Dr. Pepper",
+    "Pepsi",
+    "Joya de manzana"
+};
+
+for (int i = 0; i < sodaList.Count; i++)
+{
+    Console.WriteLine(i + " - " + sodaList[i]);
+}
+
+Console.WriteLine("Enter the selected soda: ");
+int selectedSoda = Convert.ToInt32(Console.ReadLine());
+
+switch (selectedSoda)
+{
+    case 0:
+        Console.WriteLine(sodaList[0] + " - 2 USD");
+        break;
+    case 1:
+        Console.WriteLine(sodaList[1] + " - 1 USD");
+        break;
+    case 2:
+        Console.WriteLine(sodaList[2] + " - 1.5 USD");
+        break;
+    case 3:
+        Console.WriteLine(sodaList[1] + " - 1.99 USD");
+        break;
+    case 4:
+        Console.WriteLine(sodaList[1] + " - 1.5 USD");
+        break;
+    default:
+        Console.WriteLine("ERROR: You did not select a soda or you entered an incorrect value.");
+        break;
+}
