652fe817b8b551716865b3ba86f94aa7598e3f98
Diego Briones <dbrionesmtz@yahoo.com>
2022-01-27 12:18:41 -0600

    ciclo for

---
+++ ForCicle/ForCicle/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ciclo for\n");
+
+for (int i = 1; i <= 50; i++)
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
